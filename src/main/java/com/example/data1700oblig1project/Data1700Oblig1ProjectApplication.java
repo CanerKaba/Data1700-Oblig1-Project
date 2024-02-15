d7bf359399031a7cff894a55c8925a0f3ecc9757
Caner Kabacaoglu <canerkbcgl@gmail.com>
2024-02-15 15:53:17 +0100

    Initial commit

---
+++ src/main/java/com/example/data1700oblig1project/Data1700Oblig1ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.data1700oblig1project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data1700Oblig1ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Data1700Oblig1ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/data1700oblig1project/Data1700Oblig1ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data1700oblig1project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data1700Oblig1ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
